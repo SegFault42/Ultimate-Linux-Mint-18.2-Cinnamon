614ad0a80b25819d884f53c8b65418be7e3a40bf
SegFault42 <rabougue@student.42.fr>
2017-10-01 16:03:41 +0200

    update

--- set_colorscheme.sh
+++ set_colorscheme.sh
@@ -1,6 +1,14 @@
 git clone http://github.com/sjl/badwolf.git
+git clone https://github.com/whatyouhide/vim-gotham.git
+
 cp -fr ./badwolf/colors/badwolf.vim ~/.vim/colors/
+cp -fr ./vim-gotham/colors/gotham256.vim ~/.vim/colors/
+
 echo "\033[32mcopied\033[0m"
+
 sed -i s/molokai/badwolf/g ~/.vimrc
+
 echo "\033[32mset badwolf in vimrc ok\033[0m"
+
 rm -fr ./badwolf
+rm -fr ./vim-gotham256
